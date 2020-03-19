51264d68a9da35e2e3333c1e7e14de16b214519a
Rafael de Mesquita Moura <rafael.mesquita@softcube.com.br>
2020-03-19 20:34:35 -0300

    Commit Inicial do Projeto

---
+++ src/main/java/br/com/softcube/microservices/loja/LojaMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.softcube.microservices.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/softcube/microservices/loja/LojaMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softcube.microservices.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
