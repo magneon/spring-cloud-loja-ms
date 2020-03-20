1efb14bff802509a08ce6831c4ff3a5cf7f07699
Rafael de Mesquita Moura <rafael.mesquita@softcube.com.br>
2020-03-19 22:52:14 -0300

    Criação do serviço de Compra

---
+++ src/main/java/br/com/softcube/microservices/loja/controllers/dtos/ItemPedidoDTO.java
@@ -0,0 +1,24 @@
+package br.com.softcube.microservices.loja.controllers.dtos;
+
+public class ItemPedidoDTO {
+
+    private Long id;
+    private int quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+}
---
+++ src/main/java/br/com/softcube/microservices/loja/controllers/dtos/PedidoDTO.java
@@ -0,0 +1,25 @@
+package br.com.softcube.microservices.loja.controllers.dtos;
+
+import java.util.List;
+
+public class PedidoDTO {
+
+    private List<ItemPedidoDTO> itens;
+    private EnderecoDTO endereco;
+
+    public List<ItemPedidoDTO> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedidoDTO> itens) {
+        this.itens = itens;
+    }
+
+    public EnderecoDTO getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(EnderecoDTO endereco) {
+        this.endereco = endereco;
+    }
+}
